49dd257b8d0dac54a9f9d56fad17e53d1c14fbaa
Swr96 <theswr1996@googlemail.com>
2021-07-01 16:36:58 +0200

    popup fixed to coordinates, marker image included

--- JS/map.js
+++ JS/map.js
@@ -17,6 +17,17 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(pos) {
         loc = [pos.coords.longitude, pos.coords.latitude]
         console.log();
+      /*  $.ajax({
+           url: 'http://localhost:8081/incidence',
+           data: JSON.stringify({long: loc[0], lat: loc[1]}),
+           dataType: "json",
+           headers:{
+               "content-type":"application/json"
+           },
+           success: function(data){
+               console.log(data)
+           }
+       }); */
         map.setView(new ol.View({
             center: ol.proj.fromLonLat(loc),
             zoom: 10
@@ -24,16 +35,37 @@ if (navigator.geolocation) {
     });
 }
 
-var layer = new ol.layer.Vector({
-        source: new ol.source.Vector({
-            features: [
-                new ol.Feature({
-                    geometry: new ol.geom.Point(ol.proj.fromLonLat([8.4036527, 49.0068901]))
-                })
-            ]
-        })
+var styles = {
+    'icon': new ol.style.Style({
+        image: new ol.style.Icon({
+            anchor: [0.5, 1],
+            src: 'marker.png',
+        }),
+    }),
+};
+
+var startMarker = new ol.Feature({
+    type: 'icon',
+    geometry: new ol.geom.Point(ol.proj.fromLonLat([8.4036527, 49.0068901])),
 });
 
+var layer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        features: [
+            startMarker
+            /*   new ol.Feature({
+                    type: 'icon',
+                    geometry: new ol.geom.Point(ol.proj.fromLonLat([8.4036527, 49.0068901]))
+                }) */
+        ],
+    }),
+    style: function(feature){
+        return styles[feature.get('type')];
+    }
+});
+
+
+
+
+
 map.addLayer(layer);
-
-
--- JS/popup.js
+++ JS/popup.js
@@ -2,31 +2,56 @@ var container = document.getElementById('popup');
 var content = document.getElementById('popup-content');
 var closer = document.getElementById('popup-closer');
 
+
+var popup = new ol.Overlay({
+    element: container
+});
+map.addOverlay(popup);
+
+map.on('click', function(e) {
+    let p = startMarker.getGeometry().flatCoordinates;
+
+    if(map.hasFeatureAtPixel(e.pixel)) {
+        popup.setPosition(p);
+       
+        // TODO: geklickten Marker bestimmen
+        // TODO: Daten im Popup ändern
+    }
+});
+
+closer.onclick = function() {
+    popup.setPosition(undefined);
+    closer.blur();
+    return false;
+};
+
+/*
 var overlay = new ol.Overlay({
-  element: container,
-  autoPan: true,
-  autoPanAnimation:{
-    duration: 250
-  }
+    element: container,
+    autoPan: true,
+    autoPanAnimation: {
+        duration: 250
+    }
 });
 
 map.addOverlay(overlay);
 
 closer.onclick = function() {
-  overlay.setPosition(undefined);
-  closer.blur();
-  return false;
+    overlay.setPosition(undefined);
+    closer.blur();
+    return false;
 };
 
-map.on('singleclick', function (event){
-  if (map.hasFeatureAtPixel(event.pixel) == true){
-    var coordinate = event.coordinate;
+map.on('singleclick', function(event) {
+    if (map.hasFeatureAtPixel(event.pixel) == true) {
+        var coordinate = event.coordinate;
 
-    content.innerHTML = '<b> Hello World!</b><br />I am a popup.';
-    overlay.setPosition(coordinate);
-  }
-  else{
-    overlay.setPosition(undefined);
-    close.blur();
-  }
-});
+        console.log(coordinate);
+
+        content.innerHTML = '<b> Hello World!</b><br />I am a popup.';
+        overlay.setPosition(coordinate);
+    } else {
+        overlay.setPosition(undefined);
+        close.blur();
+    }
+}); */
