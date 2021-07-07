9d833410fcf1b328b19c03f6fc93e51187efb255
nkilders <noah.kilders@gmail.com>
2021-07-07 09:56:32 +0200

    Update popup.js

--- JS/popup.js
+++ JS/popup.js
@@ -8,15 +8,53 @@ var popup = new ol.Overlay({
 });
 map.addOverlay(popup);
 
-map.on('click', function(e) {
-    let p = startMarker.getGeometry().flatCoordinates;
+/**
+ * Lädt eine XML-basierte Datei von einer URL
+ * 
+ * @param {String} url URL zur Datei
+ * @param {Function} callback Wird aufgerufen sobald die Datei erfolgreich geladen wurde. Der Inhalt der Datei wird als Funktionsparameter übergeben.
+ */
+function load(url, callback) {
+    let req = new XMLHttpRequest();
+    req.open('GET', url);
+    
+    try {
+        req.responseType =  'document';
+    } catch(ex) {}
 
-    if(map.hasFeatureAtPixel(e.pixel)) {
-        popup.setPosition(p);
-       
-        // TODO: geklickten Marker bestimmen
-        // TODO: Daten im Popup ändern
+    req.onload = function() {
+        callback(r.responseXML);
+    };
+
+    req.send();
+}
+
+map.on('click', function(e) {
+    // Abbrechen, wenn an der geklickten Stelle kein Marker ist
+    if(!map.hasFeatureAtPixel(e.pixel)) return;
+
+    let p = startMarker.getGeometry().flatCoordinates;
+    popup.setPosition(p);
+
+    p = ol.proj.toLonLat(p);
+
+    if(typeof XSLTProcessor != 'undefined') {
+        console.error('XSLTProcessor not found!\nCannot perform XSL-Transformation')
+        return;
     }
+
+    // TODO: hier sollten die Daten zum geklickten Test-/Impfzentrum geladen werden, nicht die Inzidenz
+    load('http://localhost:8081/incidence?long=' + p[0] + '&lat=' + p[1], function(xml) {
+        load('http://localhost:8081/xml/inzidenz.xsl', function(xsl) {
+            let processor = new XSLTProcessor();
+            processor.importStylesheet(xsl);
+
+            let fragment = processor.transformToFragment(xml, document);
+            
+            content.innerHTML = '';
+            content.appendChild(fragment);
+        });
+    });
 });
 
 closer.onclick = function() {
